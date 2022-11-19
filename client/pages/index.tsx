72ecbeb551f39668e5a6f1de3d7c5f265402d207
fedeloterstein <federicoloterstein@gmail.com>
2022-11-19 19:42:21 -0300

    add google analytics

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,29 +5,44 @@ import { DataDisplay, DataTransactions } from '../components'
 import { useContractEvent } from 'wagmi'
 import abi from '../contract/abi.json'
 import { utils } from 'ethers'
+import Script from 'next/script'
 export default function Home() {
-
-
   useContractEvent({
     address: '0x45ccC035B8bEf1896104C19105c1203dB7e001Ed',
     abi: abi.abi,
     eventName: 'NewInitiateTransaction',
-    listener( _to, _from, _amount) {
-      alert(` To: ${_to}, From: ${_from}, Amount: ${utils.formatEther(_amount)}`)
+    listener(_to, _from, _amount) {
+      alert(
+        ` To: ${_to}, From: ${_from}, Amount: ${utils.formatEther(_amount)}`,
+      )
       location.reload()
     },
   })
 
-  
   return (
     <div>
       <Head>
         <title>FEVM-2FA Wallet 🔐</title>
-        <meta name="description" content="Project built in the ETHGlobal FEVM Hackathon" />
+        <meta
+          name="description"
+          content="Project built in the ETHGlobal FEVM Hackathon"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Container padding={10} maxW={'container.lg'}>
+      <Script
+      async src="https://www.googletagmanager.com/gtag/js?id=G-D95QZF84HQ"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-D95QZF84HQ');
+        `}
+      </Script>
+
         <ConnectButton />
         <DataDisplay />
         <DataTransactions />
